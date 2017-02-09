1045c5397a9b0af459dd486cd808e6ec3bd6a752
Javier <javierurquizu@gmail.com>
2017-02-09 11:38:34 -0800

    Developed Home Page with 2 sample highlight projects. Next step is to add a 'Start' button, and also to have each project's name be a hyperlink to the project's page.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@
  * GET home page.
  */
 
+// Get all of our project data
+var projectData = require('../projectData.json');
+//var data = require('../data.json');
+
 exports.view = function(req, res){
-  res.render('index');
+	console.log(projectData);	// Test that we're really reading the data
+	res.render('index', projectData);
 };
