026f9a44e379e192fd2b5e5c45ae14fd2bd84d1b
Javier <javierurquizu@gmail.com>
2017-02-16 12:53:30 -0800

    Began mergining navbar with older elements. Also implemented first draft of login screen. Still need to create importable header and footer files.

--- public/js/comeTogether.js
+++ public/js/comeTogether.js
@@ -12,44 +12,27 @@ function initializePage() {
 	console.log("Javascript connected!");
 	//$("a.nameClick").click(replaceName);
 }
-/**
-function replaceName(event) {
-	event.preventDefault()
-	var curName = $(this).text();
-	var newName = anagrammedName(curName);
-	$(this).text(newName);
-}
 
-function anagrammedName(name) {
-	// Thanks, Internet Anagram Server!
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 	
-	if (name == "Doug Engelbart") {
-		return "Notable Grudge";
-	} 
-	else if (name == "Ivan Sutherland") {
-		return "Vandal Heist Run";
-	}
-	else if (name == "JCR Licklider") {
-		return "Crick Rid Jell";
-	}
-	else if (name == "Vannevar Bush") {
-		return "Ravens Van Hub";
-	}
-	else if (name == "Alan C. Kay") {
-		return "Canal Yak";
-	}
-	else if (name == "Allen Newell") {
-		return "Ellen All New";
-	}
-	else if (name == "Lucy Suchman") {
-		return "Lunacy Chums";
-	}
-	else if (name == "Grace Hopper") {
-		return "Gear Chopper";
-	}
-	else {
-		console.log(name + " not known for anagramming.");
-		return name;
-	}
+
 }
-*/
