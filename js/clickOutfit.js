12e64728e377f8b73c4e502f6be2ffca13faa060
Jingya Huang <jih201@ucsd.edu>
2017-02-22 23:30:11 -0800

    Fix the click function by direct adding script in dashboard.handlerbar

---
+++ js/clickOutfit.js
@@ -0,0 +1,17 @@
+var indexI = 0;
+var indexJ = 0;
+
+function clickShirt(e) {
+	var shirt = ["/images/shirt1.png","/images/shirt2.png","/images/shirt3.png","/images/shirt4.png"];
+	var x = document.getElementById("Shirt");
+	indexI += 1;
+	x.setAttribute( "src", shirt[indexI] );
+
+}
+
+function clickPants(e) {
+	var pants = ["/images/pant1.png","/images/pant2.png","/images/pant3.png","/images/pant4.png"];
+	var y = document.getElementById("Pants");
+	indexJ += 1;
+	y.setAttribute( "src", pants[indexJ] );
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,24 +2,7 @@
 /*
  * GET home page.
  */
-var indexI = 0;
-var indexJ = 0;
 
 exports.view = function(req, res){
 	res.render('dashboard');
 };
-
-function clickShirt(e) {
-	var shirt = ["/images/shirt1.png","/images/shirt2.png","/images/shirt3.png","/images/shirt4.png"];
-	var x = document.getElementById("Shirt");
-	indexI += 1;
-	x.setAttribute( "src", shirt[indexI] );
-
-}
-
-function clickPants(e) {
-	var pants = ["/images/pant1.png","/images/pant2.png","/images/pant3.png","/images/pant4.png"];
-	var y = document.getElementById("Pants");
-	indexJ += 1;
-	y.setAttribute( "src", pants[indexJ] );
-}
