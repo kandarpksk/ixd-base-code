63d42bbe2bfcc88283775466f3ebeedb0fc7134e
Riyang Liu <riyangl@student.ucsd.edu>
2017-02-22 22:29:28 -0800

    adding google vision and changing closetdata.json

--- routes/closet.js
+++ routes/closet.js
@@ -1,5 +1,36 @@
 var closetdata = require('../closetdata.json');
+var fs = require('fs')
 
 exports.view = function(req, res){
   res.render('closet', closetdata);
 };
+
+const Vision = require('@google-cloud/vision');
+const projectId='ewear-159500';
+const visionClient = Vision({
+	projectId: projectId
+});
+
+closetdata.photos.forEach(function(element){ 
+	fileName = element.imageURL;
+	console.log(fileName);
+
+	visionClient.detectLabels(fileName)
+	.then((results) => {
+
+		const labels = results[0];
+		console.log('Labels:');
+		labels.forEach((label) => console.log(label));
+
+	    //fs.readFile('like.json', function (err, data) {
+   	    //var json = JSON.parse(data);
+   	    //json.like.push(labels);
+
+   	    //fs.writeFile("like.json", JSON.stringify(json))
+    
+    //})
+
+	})
+
+});
+
